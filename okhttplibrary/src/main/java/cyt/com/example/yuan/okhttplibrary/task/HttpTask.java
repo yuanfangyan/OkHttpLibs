b670ce5c5488ad4148d62ddd0271a683e743da6f
yuan <1225418966@qq.com>
2018-08-06 16:37:46 +0800

    2018/8/6

---
+++ okhttplibrary/src/main/java/cyt/com/example/yuan/okhttplibrary/task/HttpTask.java
@@ -0,0 +1,97 @@
+package cyt.com.example.yuan.okhttplibrary.task;
+
+import android.support.annotation.Nullable;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.Executor;
+
+import cyt.com.example.yuan.okhttplibrary.exector.HttpThread;
+import cyt.com.example.yuan.okhttplibrary.listener.ResponseListener;
+import okhttp3.MediaType;
+import okhttp3.RequestBody;
+
+public class HttpTask<T> {
+    private static String url;
+    private static Map<String, String> headers;
+    private Executor mainExecutor;
+    private static Map<String, String> bodys;
+    private HttpThread<T> httpThread;
+
+    public HttpThread<T> getHttpThread() {
+        return httpThread;
+    }
+
+    private MediaType from_type = MediaType.parse("text/x-markdown; charset=utf-8");
+    private static RequestBody requestBody;
+    private ResponseListener<T> responseListener;
+
+    public HttpTask(String url, Map<String, String> headers, Map<String, String> bodys, ResponseListener<T> responseListener, Executor mainExecutor) {
+        this.url = url;
+        this.headers = headers;
+        this.bodys = bodys;
+        this.responseListener = responseListener;
+        this.mainExecutor = mainExecutor;
+        requestBody = RequestBody.create(from_type, getPostBody());
+        httpThread = new HttpThread<>(this);
+    }
+
+    @Nullable
+    private String getPostBody() {
+        if (bodys == null) {
+            return null;
+        } else {
+            Set<Map.Entry<String, String>> set = bodys.entrySet();
+            StringBuffer stringBuffer = new StringBuffer();
+            int size = 0;
+            for (Map.Entry<String, String> entry : set) {
+                if (size != 0) {
+                    stringBuffer.append("&");
+                }
+                size++;
+                stringBuffer.append(entry.getKey());
+                stringBuffer.append("=");
+                stringBuffer.append(entry.getValue());
+            }
+            return stringBuffer.toString();
+        }
+
+    }
+
+    public static String getUrl() {
+        return url;
+    }
+
+    public static Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public static RequestBody getRequestBody() {
+        return requestBody;
+    }
+
+
+    public void success(final T t) {
+        if (responseListener == null) {
+            return;
+        }
+        mainExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                responseListener.responseSuccess(getUrl(), t);
+            }
+        });
+    }
+
+    public void fail(final String err) {
+        if (responseListener == null) {
+            return;
+        }
+        mainExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                responseListener.responseFail(getUrl(), err);
+            }
+        });
+    }
+}
